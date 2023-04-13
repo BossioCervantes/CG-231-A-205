ac161368d49ceb076a2701ceb1ae244976a2f9b7
BossioCervantes <124269531+BossioCervantes@users.noreply.github.com>
2023-04-13 10:15:19 -0500

    Update paralelo.js

--- paralelo.js
+++ paralelo.js
@@ -1,3 +1,8 @@
+/*  Escriba la definición de una función en javascript que reciba un
+    vector 3D cualquiera como parámetro (interpretarlo como mejor lo considere)
+    y retorne si el mismo es o no paralelo a uno de los ejes principales
+    (X, Y o Z).*/
+
 function vector(v){
 
     if (v.x !==0 && v.y ==0 && v.z ==0)
@@ -10,3 +15,15 @@ function vector(v){
     {return true;} // Es paralelo entre X y Y
    
    }
+
+/*  Escriba la definición de una función en javascript que reciba
+    un arreglo de elementos de tipo entero y retorne el elemento de 
+    menor valor. */
+
+let disminuir = arreglo[0];
+for (int i = 1; i < arreglo.lenght; i++){
+	if (arreglo[i] < disminuir {
+	  disminuir = arreglo;
+	}
+}
+
