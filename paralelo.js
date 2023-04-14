1a0ff43378a5b75a5895fd56c08ce2044ef41642
BossioCervantes <124269531+BossioCervantes@users.noreply.github.com>
2023-04-13 19:16:15 -0500

    Update paralelo.js

--- paralelo.js
+++ paralelo.js
@@ -6,13 +6,13 @@
 function vector(v){
 
     if (v.x !==0 && v.y ==0 && v.z ==0)
-    {return true;} // Es paralelo entre Y y Z
+    {return true;} // Es paralelo en X
    
     if (v.x ==0 && v.y !==0 && v.z ==0)
-    {return true;} // Es paralelo entre X y Z
+    {return true;} // Es paralelo en Y
    
     if (v.x ==0 && v.y ==0 && v.z !==0)
-    {return true;} // Es paralelo entre X y Y
+    {return true;} // Es paralelo en Z
     
 return false;
 }
