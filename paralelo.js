843e9065a78bf62f45c05dce49023d1aa373d849
BossioCervantes <124269531+BossioCervantes@users.noreply.github.com>
2023-04-13 20:07:03 -0500

    Update paralelo.js

--- paralelo.js
+++ paralelo.js
@@ -3,30 +3,33 @@
     y retorne si el mismo es o no paralelo a uno de los ejes principales
     (X, Y o Z).*/
 
-function vector(v){
-
-    if (v.x !==0 && v.y ==0 && v.z ==0)
+function vector(v){   
+	
+    if (v.x !== 0 && v.y ===0 && v.z ===0)
     {return true;} // Es paralelo en X
    
-    if (v.x ==0 && v.y !==0 && v.z ==0)
+    else if (v.x === 0 && v.y !== 0 && v.z === 0)
     {return true;} // Es paralelo en Y
    
-    if (v.x ==0 && v.y ==0 && v.z !==0)
+    else if (v.x === 0 && v.y === 0 && v.z !== 0)
     {return true;} // Es paralelo en Z
     
-return false;
+   return false;
 }
-
 
 /*  Escriba la definición de una función en javascript que reciba
     un arreglo de elementos de tipo entero y retorne el elemento de 
     menor valor. */
+
+
 function disminuir_arreglo(arreglo)
 {
 let disminuir = arreglo[0];
-for (int i = 1; i < arreglo.lenght; i++){
-	if (arreglo[i] < disminuir) {
-	  disminuir = arreglo;
+for (let i = 1; i < arreglo.lenght; i++){
+	
+	if (arreglo[i] < disminuir){
+		
+	  disminuir = arreglo[i];
 	}
      }
    return disminuir;
